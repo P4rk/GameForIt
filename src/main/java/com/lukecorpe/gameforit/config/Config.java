102c0ab9c46b6cef687db3d7b0a21b0aa06abcf7
Luke Corpe <lukecorpe@gmail.com>
2014-09-01 19:41:47 +0100

    added resource handler

--- src/main/java/com/lukecorpe/gameforit/config/Config.java
+++ src/main/java/com/lukecorpe/gameforit/config/Config.java
@@ -3,9 +3,16 @@ package com.lukecorpe.gameforit.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan(basePackages = {"com.lukecorpe.gameforit"})
 @Import({ViewConfig.class})
-public class Config {
+public class Config extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/","classpath:/static/");
+    }
 }
